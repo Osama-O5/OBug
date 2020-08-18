ff1008751e19dad9bf7266086556d71a57caee93
Osama-O5 <62245274+Osama-O5@users.noreply.github.com>
2020-08-18 15:54:31 +0200

    commit

--- src/com/ALM.java
+++ src/com/ALM.java
@@ -11,6 +11,6 @@ public class ALM extends obug {
         obug.Caps("webdriver.chrome.driver", "\\drivers\\chromedriver.exe");
         obug.Login("EG_aalaa2", "ahly@myVFc0re");
         obug.URlQuery("Enterprise_prod_and_serv", "mCare_core", "", "", "", "'Major'", "", "", ">='2020-06-8'");
-        obug.fullRegressionPack();
+        obug.fullRegressionPack("Regression");
     }
 }
